33aa8872863ef21af63e3b68c5fba04a9e6a1a75
swananana <swananana@users.noreply.github.com>
2022-03-08 02:13:56 +0530

    final changes, all test cases work.

--- src/main/java/powerpackage/PowerfinderTest.java
+++ src/main/java/powerpackage/PowerfinderTest.java
@@ -1,29 +1,29 @@
 package powerpackage;
 import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-
-public class Powertest {
+public class PowerfinderTest {
     @Test
-    public void one_raised_to_one_is_onew() {
+    public void One_raised_to_one_is_onew() {
 
         assert Powerfinder.oF(1,1) == 1;
 
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
+    public void TwoRaised_to_one_is_2() {
 
         assert Powerfinder.oF(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
+    public void Two_power_2_is_4() {
 
         assert Powerfinder.oF(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
+    public void Power_of_2_and_3_Is_SIX() {
 
         assert Powerfinder.oF(3, 2) == 9;
     }
